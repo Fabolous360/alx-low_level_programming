1791256ddbfaa15030858ab98038046cab691dcd
Lolade <loladeadeyeye@gmail.com>
2022-12-08 22:53:20 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#Include <stdio>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
